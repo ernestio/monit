283eefa7f996a3d4a856062f3f433210efea0828
Tom Bevan <tom.bevan@r3labs.io>
2017-09-15 12:28:36 +0100

    updated event subjects

--- service.go
+++ service.go
@@ -40,11 +40,11 @@ func processService(msg *nats.Msg) {
 	}
 
 	switch msg.Subject {
-	case "service.create", "service.delete", "service.import":
+	case "build.create", "build.delete", "build.import":
 		log.Println("Creating stream: ", id)
 		ss.CreateStream(id)
 		ss.Publish(id, &sse.Event{Data: data})
-	case "service.create.done", "service.create.error", "service.delete.done", "service.delete.error", "service.import.done", "service.import.error":
+	case "build.create.done", "build.create.error", "build.delete.done", "build.delete.error", "build.import.done", "build.import.error":
 		ss.Publish(id, &sse.Event{Data: data})
 		go func(ss *sse.Server) {
 			// Wait for any late connecting clients before closing stream
